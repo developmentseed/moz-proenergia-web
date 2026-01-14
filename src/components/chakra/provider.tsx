162bfa2e817cf226dbd635ba73795a455a5dd5be
Hanbyul Jo <hanbyul@nyu.edu>
2026-01-14 13:01:16 -0500

    Turn off color mode provider for now

--- src/components/chakra/provider.tsx
+++ src/components/chakra/provider.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
-} from "./color-mode"
+} from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      {props.children}
+      {/* <ColorModeProvider {...props} /> */}
     </ChakraProvider>
-  )
+  );
 }
