a72dd7b7ceb8077a12d3f4975b6fe2b99080166b
Hanbyul Jo <hanbyul@nyu.edu>
2026-01-23 10:50:12 -0500

    Add about , download page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,37 @@
-import { Container } from "@chakra-ui/react";
+import { type ReactNode } from "react";
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+import AboutContent from './about.mdx';
+
+interface ChildrenType {
+  children: ReactNode
+}
+
+function H1({ children }: ChildrenType) {
+  return <Heading as='h1' mb={2}>{children}</Heading>;
+}
+
+function H2({ children }: ChildrenType) {
+  return <Heading as='h2' mb={2}>{children}</Heading>;
+}
+
+function H3({ children }: ChildrenType) {
+  return <Heading as='h3' mb={2}>{children}</Heading>;
+}
+
+function Paragraph ({ children }: ChildrenType) {
+  return <Text mb={4}>{children}</Text>;
+}
+
+const overrideComponents = {
+  h2: H2,
+  h3: H3,
+  p: Paragraph
+};
 
 export default function Page() {
   return <Container>
-    <h1>About page test</h1>
+    <Box mt={8} mb={8}>
+      <AboutContent components={overrideComponents} />
+    </Box>
   </Container>;
 }
---
+++ src/app/downloads/page.tsx
@@ -0,0 +1,5 @@
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+
+export default function Page ({}) {
+  return <Container>Download page</Container>;
+}
