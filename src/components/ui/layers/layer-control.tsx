b723c7f1169adf1223d4f0e0a59d4039239446e6
Hanbyul Jo <hanbyul@nyu.edu>
2026-01-27 14:18:37 -0500

    Type fix

--- src/components/ui/layers/layer-control.tsx
+++ src/components/ui/layers/layer-control.tsx
@@ -21,8 +21,8 @@ export const LayerControl = ({ layer, onChange, selected }: LayerControlProps) =
         <InfoTip content="This is some additional information." />
       </Box>
       <Box display="flex" alignItems="center" gap="2">
-
-        <DownloadButton url={layer.downloadLink} label={`Download ${layer.label}`} />
+        {/* TODO: properly render download button  */}
+        <DownloadButton url={layer.downloadLink || ""} label={`Download ${layer.label}`} />
 
         <Switch.Root value={layer.id} onCheckedChange={onCheckedChange} checked={selected}>
           <Switch.HiddenInput />
