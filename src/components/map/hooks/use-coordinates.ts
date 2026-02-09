c6a455f41fc8fd8851bf9ebf67e3cd652508376b
LanesGood <lane@developmentseed.org>
2026-02-09 14:43:16 -0500

    Update base map state coords to fit whole country

--- src/components/map/hooks/use-coordinates.ts
+++ src/components/map/hooks/use-coordinates.ts
@@ -1,9 +1,8 @@
 'use client';
 import { useQueryStates, parseAsFloat, throttle } from 'nuqs';
 
-//const COORDS = [-25.9692, 32.5732];
-const COORDS = [-15.43422, 31.35793];
-const ZOOM = 7;
+const COORDS = [-18.841,35.57];
+const ZOOM = 5.5;
 
 export function useCoordinates() {
   return useQueryStates({
