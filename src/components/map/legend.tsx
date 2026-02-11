f475c0852c471cc2990fbf1d65910159e044237f
Hanbyul Jo <hanbyul@nyu.edu>
2026-02-11 14:31:33 -0500

    Put additional layer in scroll area

--- src/components/map/legend.tsx
+++ src/components/map/legend.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack, HStack, Text, Separator } from '@chakra-ui/react';
+import { Box, VStack, HStack, Text, Separator, ScrollArea } from '@chakra-ui/react';
 import { type MapItemUnit } from '@/app/types';
 import { useContextualLayers } from '@/utils/context/contextual-layers';
 import { controlZIndex, mapControlCommonStyleProps } from './control-constant';
@@ -39,19 +39,30 @@ export function Legend({ items }: LegendProps) {
         {contextualLayers.length > 0 && (
           <>
             <Separator />
-            {contextualLayers.map((layer) => (
-              <HStack key={layer.id} gap={2}>
-                <Box
-                  w="3"
-                  h="3"
-                  bg={layer.color}
-                  borderRadius="100%"
-                  border='1px solid black'
-                  flexShrink={0}
+            <ScrollArea.Root size="xs">
+              <ScrollArea.Viewport>
+                <ScrollArea.Content spaceY="4" textStyle="sm">
+                  <Text textStyle='tableAttr' mb={1}>
+                    Additional layers
+                  </Text>
+                  {contextualLayers.map((layer) => (
+                    <HStack key={layer.id} gap={2} maxHeight='64'>
+                      <Box
+                        w="3"
+                        h="3"
+                        bg={layer.color}
+                        borderRadius="100%"
+                        border='1px solid black'
+                        flexShrink={0}
                 />
-                <Text textStyle='tableValue'>{layer.label}</Text>
-              </HStack>
+                      <Text textStyle='tableValue'>{layer.label}</Text>
+                    </HStack>
             ))}
+                </ScrollArea.Content>
+              </ScrollArea.Viewport>
+              <ScrollArea.Scrollbar orientation="vertical" />
+              <ScrollArea.Corner bg="bg" />
+            </ScrollArea.Root>
           </>
         )}
       </VStack>
