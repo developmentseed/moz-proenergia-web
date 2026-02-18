485edc840c304c8cd972808c8a7bd9df23992ea0
Hanbyul Jo <hanbyul@nyu.edu>
2026-02-18 10:14:45 -0500

    Fix additional layer legend style

--- src/components/map/legend.tsx
+++ src/components/map/legend.tsx
@@ -42,22 +42,24 @@ export function Legend({ items }: LegendProps) {
             <ScrollArea.Root size="xs">
               <ScrollArea.Viewport>
                 <ScrollArea.Content spaceY="4" textStyle="sm">
-                  <Text textStyle='tableAttr' mb={1}>
-                    Additional layers
-                  </Text>
-                  {contextualLayers.map((layer) => (
-                    <HStack key={layer.id} gap={2} maxHeight='64'>
-                      <Box
-                        w="3"
-                        h="3"
-                        bg={layer.color}
-                        borderRadius="100%"
-                        border='1px solid black'
-                        flexShrink={0}
+                  <Box>
+                    <Text textStyle='tableAttr' mb={1}>
+                      Additional layers
+                    </Text>
+                    {contextualLayers.map((layer) => (
+                      <HStack id='hstsack' key={layer.id} gap={2} mt={1}>
+                        <Box
+                          w="3"
+                          h="3"
+                          bg={layer.color}
+                          borderRadius="100%"
+                          border='1px solid black'
+                          flexShrink={0}
                 />
-                      <Text textStyle='tableValue'>{layer.label}</Text>
-                    </HStack>
+                        <Text textStyle='tableValue'>{layer.label}</Text>
+                      </HStack>
             ))}
+                  </Box>
                 </ScrollArea.Content>
               </ScrollArea.Viewport>
               <ScrollArea.Scrollbar orientation="vertical" />
