212aaad7f9b7e94eaf94b1c3ccf4cceaf371802b
Hanbyul Jo <hanbyul@nyu.edu>
2026-08-12 09:42:45 -0400

    Add pt as default

--- src/i18n/query.ts
+++ src/i18n/query.ts
@@ -3,6 +3,6 @@ import { createSerializer, parseAsStringLiteral } from 'nuqs/server';
 export const languages = ['en', 'pt'] as const;
 export type Language = (typeof languages)[number];
 
-export const langParser = parseAsStringLiteral(languages);
+export const langParser = parseAsStringLiteral(languages).withDefault(languages[1]);
 
 export const serializeLang = createSerializer({ lang: langParser });
